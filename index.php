d0059ce8db14d01fda904cccf21fb609d841aca2
dixmod <dixmod@gmail.com>
2018-07-31 21:17:24 +0300

    ComposerSum

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-function sum($a, $b)
-{
-    $s = $a + $b;
-    return "dixmod says $a + $b = $s" . PHP_EOL;
-}
+include 'src/Sum.php';
 
-$a = 10;
-$b = 15;
-
-echo sum($a, $b);
+$sum = new dixmod\Sum();
+echo $sum->sum(10, 15);
---
+++ src/Sum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace dixmod;
+
+class Sum
+{
+    function sum($a, $b)
+    {
+        $s = $a + $b;
+        return "dixmod says $a + $b = $s" . PHP_EOL;
+    }
+}
